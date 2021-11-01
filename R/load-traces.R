575f6ab2afcfa3aef3a60c3f24b5a5526f9dfc10
mpadge <mark.padgham@email.com>
2021-11-01 11:59:45 +0100

    add R/load-traces.R

---
+++ R/load-traces.R
@@ -0,0 +1,35 @@
+
+#' Load traces of parameter types
+#'
+#' @export
+load_traces <- function () {
+
+    td <- options ("typetracedir")$typetracedir
+    traces <- list.files (td, pattern = "^typetrace\\_", full.names = TRUE)
+
+    out <- lapply (traces, function (i) {
+                       xi <- brio::readLines (i)
+                       do.call (rbind, strsplit (xi, ","))
+        })
+    out <- tibble::tibble (data.frame (do.call (rbind, out)))
+
+    names (out) <- c ("function", "parameter", "storage_mode", "length")
+
+    out$length <- as.integer (out$length)
+
+    return (out)
+}
+
+#' Clear previous traces
+#'
+#' Traces are by default appended to previous traces. This function can be used
+#' to clean those previous ones, to enable subsequent calls to generate new
+#' traces that are not appended to previous ones.
+#' @export
+clear_traces <- function () {
+
+    td <- options ("typetracedir")$typetracedir
+    traces <- list.files (td, pattern = "^typetrace\\_", full.names = TRUE)
+
+    invisible (file.remove (traces))
+}
