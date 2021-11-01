706f7a37ba073949281b32dc995da4eb5bcb1d8e
mpadge <mark.padgham@email.com>
2021-11-01 11:39:22 +0100

    add inject-tracer.R

---
+++ R/inject-tracer.R
@@ -0,0 +1,14 @@
+
+#' Inject parameter tracer into one function
+#'
+#' @param f Name of function as character string
+#' @param e Environment in which function is defined; generally
+#' `as.environment("package:<pkg_name>")`.
+#' @return Nothing (will error on fail).
+#'
+#' @export
+inject_tracer <- function (f, e) {
+
+    code <- body (typetracer:::get_types)
+    invisible (inject_code (code, f))
+}
