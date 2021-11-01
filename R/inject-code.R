0256770b58b8b4fcabcd6e4bc4a971d1d75ef4f2
mpadge <mark.padgham@email.com>
2021-11-01 12:49:10 +0100

    lint original R/inject-code.R file

--- R/inject-code.R
+++ R/inject-code.R
@@ -23,57 +23,59 @@
 #' @return NULL invisibly
 #'
 #' @export
-inject_code <- function(code, fun, where="onentry", wrap=FALSE) {
-    code <- substitute(code)
+inject_code <- function (code, fun, where = "onentry", wrap = FALSE) {
+    code <- substitute (code)
 
     # this is a dispatch based on SEXP type rather than on the class attribute
     # the reason why we do not use S3 dispatch based on class is that there are
     # too many classes for calls (cf. lang2str in R/src/main/attrib.c)
-    switch(
-        typeof(code),
-        symbol=inject_symbol(code, fun, where, wrap),
-        language=inject_language(code, fun, where, wrap),
-        default=stop("Unsupported code type: ", typeof(code))
+    switch (
+        typeof (code),
+        symbol = inject_symbol (code, fun, where, wrap),
+        language = inject_language (code, fun, where, wrap),
+        default = stop ("Unsupported code type: ", typeof (code))
     )
 }
 
-inject_symbol <- function(name, fun, where, wrap) {
-    stopifnot(is.symbol(name))
+inject_symbol <- function (name, fun, where, wrap) {
+    stopifnot (is.symbol (name))
 
-    tryCatch({
-        sym <- resolve_name(name, sys.nframe()-1)
-    }, error=function(e) {
-        stop("Unable to resolve symbol `", name, "'")
+    tryCatch ({
+        sym <- resolve_name (name, sys.nframe () - 1)
+    }, error = function (e) {
+        stop ("Unable to resolve symbol `", name, "'")
     })
 
-    switch(
-        typeof(sym),
-        closure=inject_closure(sym, fun, where, wrap),
-        expression=inject_expression(sym, fun, where, wrap),
-        language=inject_language(sym, fun, where, wrap),
-        stop("Unsupported code type: ", typeof(sym))
+    switch (
+        typeof (sym),
+        closure = inject_closure (sym, fun, where, wrap),
+        expression = inject_expression (sym, fun, where, wrap),
+        language = inject_language (sym, fun, where, wrap),
+        stop ("Unsupported code type: ", typeof (sym))
     )
 }
 
-resolve_name <- function(name, pos) {
+resolve_name <- function (name, pos) {
     v <- name
-    while(pos >= 0 && is.symbol(v)) {
-        tryCatch({
-            v <- get(as.character(v), envir=sys.frame(pos), inherits=FALSE)
-        }, error=function(e) {
-            if (pos == 0) stop(e)
+    while (pos >= 0 && is.symbol (v)) {
+        tryCatch ({
+            v <- get (as.character (v),
+                      envir = sys.frame (pos),
+                      inherits = FALSE)
+        }, error = function (e) {
+            if (pos == 0) stop (e)
         })
         pos <- pos - 1
     }
     v
 }
 
-inject_expression <- function(code, fun, where, wrap) {
-    stopifnot(is.expression(code))
-    stopifnot(is.function(fun))
+inject_expression <- function (code, fun, where, wrap) {
+    stopifnot (is.expression (code))
+    stopifnot (is.function (fun))
 
     code <- if (wrap) {
-        as.call(c(as.function(as.list(code))))
+        as.call (c (as.function (as.list (code))))
     } else {
         # get the content of the EXPRSXP
         #code[[1]]
@@ -81,75 +83,82 @@ inject_expression <- function(code, fun, where, wrap) {
     }
 
     # checks if OpenCurlyBrace (i.e. `{`) has been redefined
-    ocb_remapped <- !identical(get("{", envir=environment(fun)), .Primitive("{"))
+    ocb_remapped <- !identical (get ("{", envir = environment (fun)),
+                                .Primitive ("{"))
 
-    wrapped_code <- prepare_code_to_run(code, where)
+    wrapped_code <- prepare_code_to_run (code, where)
 
-    fun_body <- body(fun)
+    fun_body <- body (fun)
 
-    if(where != "onentry") {
-        fun_body <- process_on_exit(fun_body)
+    if (where != "onentry") {
+        fun_body <- process_on_exit (fun_body)
     }
 
-    new_body <- prepend_code(fun_body, wrapped_code, ocb_remapped)
+    new_body <- prepend_code (fun_body, wrapped_code, ocb_remapped)
 
-    invisible(reassign_function_body(fun, new_body))
+    invisible (reassign_function_body (fun, new_body))
 }
 
-inject_language <- function(code, fun, where, wrap) {
-    stopifnot(typeof(code) == "language")
-    inject_expression(as.expression(code), fun, where, wrap)
+inject_language <- function (code, fun, where, wrap) {
+    stopifnot (typeof (code) == "language")
+    inject_expression (as.expression (code), fun, where, wrap)
 }
 
-inject_closure <- function(code, fun, where, wrap) {
-    stopifnot(is.function(code))
-    stopifnot(is.function(fun))
+inject_closure <- function (code, fun, where, wrap) {
+    stopifnot (is.function (code))
+    stopifnot (is.function (fun))
 
     if (wrap) {
-        warning("ignoring wrap=TRUE in inject_closure")
+        warning ("ignoring wrap = TRUE in inject_closure")
     }
 
-    code <- as.call(c(code, lapply(names(formals(fun)), as.name)))
+    code <- as.call (c (code, lapply (names (formals (fun)), as.name)))
 
-    inject_language(code, fun, where, FALSE)
+    inject_language (code, fun, where, FALSE)
 }
 
-prepend_code <- function(orig_code, code, use_primitive) {
-    # is.language will not work since SYMSXP and EXPRSXP are also of language type
-    if (typeof(orig_code) == "language" &&
-            identical(orig_code[[1]], as.name("{")) &&
+prepend_code <- function (orig_code, code, use_primitive) {
+    # is.language will not work since SYMSXP and EXPRSXP are also of language
+    # type
+    if (typeof (orig_code) == "language" &&
+            identical (orig_code[[1]], as.name ("{")) &&
             !use_primitive) {
-        as.call(append(as.list(orig_code), code, 1))
+        as.call (append (as.list (orig_code), code, 1))
     } else if (use_primitive) {
-        substitute(.Primitive("{")(CODE, ORIG_CODE), list(CODE=code, ORIG_CODE=orig_code))
+        substitute (.Primitive ("{") (CODE, ORIG_CODE),
+                    list (CODE = code, ORIG_CODE = orig_code))
     } else {
-        substitute({ CODE; ORIG_CODE }, list(CODE=code, ORIG_CODE=orig_code))
+        substitute ({ CODE; ORIG_CODE },                        # nolint
+                    list (CODE = code, ORIG_CODE = orig_code))
     }
 }
 
-prepare_code_to_run <- function(code, where) {
-    where <- match.arg(where, c("onentry", "onexit", "onerror", "onsuccess"), several.ok=FALSE)
+prepare_code_to_run <- function (code, where) {
+    where <- match.arg (where,
+                        c ("onentry", "onexit", "onerror", "onsuccess"),
+                        several.ok = FALSE)
 
-    switch(
+    switch (
         where,
-        onentry=code,
-        onexit=wrap_code_onexit(code),
-        onerror=wrap_code_onexit_state(code, failure=TRUE),
-        onsuccess=wrap_code_onexit_state(code, failure=FALSE)
+        onentry = code,
+        onexit = wrap_code_onexit (code),
+        onerror = wrap_code_onexit_state (code, failure = TRUE),
+        onsuccess = wrap_code_onexit_state (code, failure = FALSE)
     )
 }
 
-wrap_code_onexit <- function(code) {
-    call("on.exit", code, TRUE, TRUE)
+wrap_code_onexit <- function (code) {
+    call ("on.exit", code, TRUE, TRUE)
 }
 
-wrap_code_onexit_state <- function(code, failure) {
-    code <- substitute({
+wrap_code_onexit_state <- function (code, failure) {
+    code <- substitute ({
         #default <- typetracer:::.default_return_value
-        default <- utils::getFromNamespace (".default_return_value", "typetracer")
-        retv <- returnValue(default=default)
-        if (identical(default, retv) == HAS_FAILED) CODE
-    }, list(CODE=code, HAS_FAILED=failure))
+        default <- utils::getFromNamespace (".default_return_value",
+                                            "typetracer")
+        retv <- returnValue (default = default)
+        if (identical (default, retv) == HAS_FAILED) CODE
+    }, list (CODE = code, HAS_FAILED = failure))
 
-    wrap_code_onexit(code)
+    wrap_code_onexit (code)
 }
