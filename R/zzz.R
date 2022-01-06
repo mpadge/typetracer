ec7bc17fb360e7674727368dc436448f22a9b33d
mpadge <mark.padgham@email.com>
2022-01-06 18:20:46 +0100

    add .onUnload to unset options

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,13 @@
 
 # nocov start
-.onLoad <- function(libname, pkgname) { # nolint
+.onLoad <- function (libname, pkgname) { # nolint
 
     options ("typetracedir" = tempdir ())
 
+}
+
+.onUnload <- function (libname, pkgname) { # nolint
+
+    options ("typetracedir" = NULL)
 }
 # nocov end
