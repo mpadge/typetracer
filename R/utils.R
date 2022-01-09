5a6024bf9250f4cb8e0f777c6a2aae877ed061a1
mpadge <mark.padgham@email.com>
2022-01-09 22:44:15 +0100

    rm process_on_exit fn (orig. from injectr)

--- R/utils.R
+++ R/utils.R
@@ -1,25 +1,3 @@
 reassign_function_body <- function(fun, body) {
     invisible(.Call(reassign_function_body_, fun, body))
 }
-
-## TODO: Fix implementation to properly handle the complete call signature of
-## on.exit
-process_on_exit <- function(expr) {
-    if (typeof (expr) == "language") {
-        if (expr [[1]] == "on.exit") {
-            expr [[3]] <- TRUE
-            expr [[4]] <- FALSE
-        }
-        else {
-            l <- length (expr)
-            for (i in 1:l) {
-                ## NOTE: expr[[i]] is used inplace to avoid missingness
-                ##       errors in expressions of the form x[,i]
-                if (typeof (expr [[i]]) == "language") {
-                    expr [[i]] <- process_on_exit (expr[[i]])
-                }
-            }
-        }
-    }
-    expr
-}
