0c74e0e9d6c76315377dc84507db92f74814da19
mpadge <mark.padgham@email.com>
2022-01-21 13:21:35 +0100

    use separate pkg for testing
    checkmate is used here, and injecting tracers messes with active functionality

--- tests/testthat/test-trace-package.R
+++ tests/testthat/test-trace-package.R
@@ -4,18 +4,20 @@ is_gh_cov <- identical (Sys.getenv ("GITHUB_WORKFLOW"), "test-coverage")
 
 test_that("trace package", {
 
-    package <- "checkmate"
+    # test pkg selected based on smallest installed size plus latest update >
+    # 2015 or so. "praise" is also an option
+    package <- "rematch"
 
     expect_s3_class (x0 <- trace_package (package), "tbl_df")
 
-    expect_true (nrow (x0) > 50) # arbitrarily low number
+    expect_true (nrow (x0) > 5) # arbitrarily low number
     expect_identical (names (x0),
                       c ("function", "parameter", "storage_mode", "length"))
 
     expect_s3_class (x1 <- trace_package (package,
                                           types = c ("examples", "tests")),
                      "tbl_df")
-    expect_true (nrow (x1) > 50)
+    expect_true (nrow (x1) > 5)
     expect_identical (names (x1),
                       c ("function", "parameter", "storage_mode", "length"))
 
