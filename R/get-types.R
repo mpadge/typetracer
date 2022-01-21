18a143f943f5d15754c1133ce1b9f9723318d8b3
mpadge <mark.padgham@email.com>
2022-01-21 13:18:48 +0100

    improve capture of NSE pars on entry for #1

--- R/get-types.R
+++ R/get-types.R
@@ -18,32 +18,55 @@ get_types <- function () {
     fn_call <- match.call (expand.dots = TRUE)
     fn_name <- fn_call [[1]]
     pars <- as.list (fn_call [-1L]) # unevalated expressions
-    par_names <- names (pars)
     fn_env <- environment ()
 
-    for (p in par_names) {
+    fn <- match.fun (fn_name)
+    par_names <- formalArgs (fn)
 
-        if (p %in% names (fn_env)) {
-            p_eval <- tryCatch (
-                get (p, envir = fn_env),
-                error = function (e) NULL)
-        } else if (p %in% names (pars)) {
-            p_eval <- tryCatch (
-                eval (pars [[p]]),
-                error = function (e) NULL)
-        }
-
-        if (!is.null (p_eval)) {
-            p_mode <- storage.mode (p_eval)
-            p_len <- length (p_eval)
-            out <- paste0 (c (fn_name, p, p_mode, p_len), collapse = ",")
-            writeLines (out, typetracer_con)
-            rm (p_mode, p_len, out, p_eval)
-        }
+    get_p <- function (p, fn_env) {
+        tryCatch (
+            get (p, envir = fn_env, inherits = FALSE, silent = TRUE),
+            error = function (e) NULL)
     }
+    eval_p <- function (p, pars) {
+        tryCatch (
+            eval (pars [[p]]),
+            error = function (e) NULL)
+    }
+
+    classes1 <- vapply (par_names, function (p) {
+                            res <- eval_p (p, pars)
+                            if (is.null (res))
+                                res <- get_p (p, fn_env)
+                            c (class (res) [1],
+                               storage.mode (res),
+                               length (res))
+            }, character (3))
+    classes1 <- t (classes1)
+    no_class <- which (classes1 [, 1] == "NULL")
+
+    classes2 <- vapply (par_names, function (p) {
+                            res <- eval_p (p, fn_env)
+                            c (class (res) [1],
+                               storage.mode (res),
+                               length (res))
+            }, character (3))
+    classes2 <- t (classes2)
+    classes1 [no_class, ] <- classes2 [no_class, ]
+
+    classes <- data.frame (classes1)
+    colnames (classes) <- c ("class", "storage.mode", "length")
+    classes$fn_name <- as.character (fn_name)
+    classes$p <- par_names
+
+    classes <- classes [, c ("fn_name", "p", "storage.mode", "length")]
+    classes <- apply (classes, 1, function (i) {
+                          out <- paste0 (i, collapse = ",")
+                          writeLines (out, typetracer_con)
+            })
     close (typetracer_con)
 
-    rm (td, nm, fname, typetracer_con,
-        fn_call, fn_name, pars, par_names,
-        fn_env, p)
+    rm (td, nm, fn, fname, typetracer_con,
+        fn_call, fn_name, fn_env, pars, par_names,
+        get_p, eval_p, classes1, classes2, no_class)
 }
