b24a1874937dd0884a1297b86d9427469bec1ab5
mpadge <mark.padgham@email.com>
2022-07-04 16:45:22 +0200

    add get_pkg_lib_path fn for #7

--- R/utils.R
+++ R/utils.R
@@ -19,3 +19,22 @@ pkg_name_from_desc <- function (path) {
 
     as.character (read.dcf (desc) [, "Package"])
 }
+
+get_pkg_lib_path <- function (package, libs) {
+
+    lib_path <- tryCatch (
+                          find.package (package, lib.loc = libs),
+                          error = function (e) NULL
+    )
+
+    if (is.null (lib_path)) {
+        stop (
+              "Package '", package, "' is not installed. Please ",
+              "install locally, or use 'devtools::load_all()' ",
+              "before calling 'trace_package()'",
+              call. = FALSE
+        )
+    }
+
+    return (gsub (paste0 (.Platform$file.sep, package), "", lib_path))
+}
