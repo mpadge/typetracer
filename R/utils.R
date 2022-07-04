c4c9158d778f9d16a94efb1e443eba3f132c45f0
mpadge <mark.padgham@email.com>
2022-07-04 14:52:10 +0200

    add pkg_name_from_desc fn for #7

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,21 @@
 reassign_function_body <- function (fun, body) {
     invisible (.Call (reassign_function_body_, fun, body))
 }
+
+pkg_name_from_desc <- function (path) {
+
+    desc <- list.files (
+        path,
+        pattern = "DESCRIPTION",
+        recursive = TRUE,
+        full.names = TRUE
+    )
+
+    if (length (desc) < 1L) {
+        stop ("No 'DESCRIPTION' file found", call. = FALSE)
+    } else if (length (desc) > 1L) {
+        stop ("Multiple 'DESCRIPTION' files found", call. = FALSE)
+    }
+
+    as.character (read.dcf (desc) [, "Package"])
+}
