27b6dc4be665cabef923e1605fc30e0a3422aafa
mpadge <mark.padgham@email.com>
2022-07-08 11:36:17 +0200

    add read_test_trace_numbers for #9

--- R/load-traces.R
+++ R/load-traces.R
@@ -78,3 +78,37 @@ clear_traces <- function () {
 
     invisible (file.remove (traces))
 }
+
+#' Read numbers of traces at start of each test
+#'
+#' These numbers are created by the code injected by the
+#' `insert_counters_in_tests()` function in install.R.
+#'
+#' @param install_path file.path (typetracedir, package), passed here as single
+#' string.
+#' @return A `data.frame` of test names and number of traces at the start of
+#' that test. These numbers can then be used to associate traces with the
+#' specified test names in the "source" column from `load_traces()`.
+#' @noRd
+read_test_trace_numbers <- function (install_path) {
+
+    flist <- list.files (
+        install_path,
+        pattern = "^tracetest\\_",
+        full.names = TRUE
+    )
+    num_traces <- data.frame (trace_number = vapply (
+        flist,
+        function (i) as.integer (readLines (i) [1]),
+        integer (1L)
+    ))
+    num_traces$test_name <- gsub (
+        "^tracetest\\_|\\.txt$|\\.txt$",
+        "",
+        basename (rownames (num_traces))
+    )
+    num_traces <- num_traces [order (num_traces$trace_number), c ("test_name", "trace_number")]
+    rownames (num_traces) <- NULL
+
+    return (num_traces)
+}
