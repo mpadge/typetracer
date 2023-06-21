9eceb680670f71b9b1a2b9c97728395abfd7e085
mpadge <mark.padgham@email.com>
2023-06-21 18:55:38 +0200

    don't require fns in curly braces to prepend code; closes #15

--- R/prepend-code.R
+++ R/prepend-code.R
@@ -5,8 +5,13 @@
 prepend_code <- function (orig_code, code) {
     # is.language will not work since SYMSXP and EXPRSXP are also of language
     # type
-    if (typeof (orig_code) == "language" &&
-        identical (orig_code [[1]], as.name ("{"))) {
+    # if (typeof (orig_code) == "language" &&
+    #     identical (orig_code [[1]], as.name ("{"))) {
+    #     as.call (append (as.list (orig_code), code, 1))
+    # }
+
+    # fns do not need curly braces to be defined:
+    if (typeof (orig_code) == "language") {
         as.call (append (as.list (orig_code), code, 1))
     }
 }
